67dd3c2ed0fbf8e390d3ef4ada2637abe293002c
unknown <ladjrafi_zakaria@yahoo.com>
2025-05-08 16:52:27 +0200

    created a global exception handler

---
+++ src/main/java/com/example/social_network_api/exception/GlobalExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.example.social_network_api.exception;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+	@ExceptionHandler(RuntimeException.class)
+	public ResponseEntity<Map<String, Object>> handleRuntimeException(RuntimeException ex) {
+		Map<String, Object> body = new HashMap<>();
+		body.put("timestamp", LocalDateTime.now());
+		body.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
+		body.put("error", "Internal Server Error");
+		body.put("message", ex.getMessage());
+		 return new ResponseEntity<>(body, HttpStatus.INTERNAL_SERVER_ERROR);
+		
+	}
+	
+}
