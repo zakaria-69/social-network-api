467eb41cb650ab55419c43eca161898131ce6edc
unknown <ladjrafi_zakaria@yahoo.com>
2025-05-08 16:56:57 +0200

    added Bean to encode password with bcrypt

--- src/main/java/com/example/social_network_api/security/SecurityConfig.java
+++ src/main/java/com/example/social_network_api/security/SecurityConfig.java
@@ -3,6 +3,8 @@ package com.example.social_network_api.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -15,4 +17,10 @@ public class SecurityConfig {
 		.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
 		return http.build();
 	}
+	
+	 @Bean
+	 public PasswordEncoder passwordEncoder() {
+		 return new BCryptPasswordEncoder();
+		 
+	 }
 }
